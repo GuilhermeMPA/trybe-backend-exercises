d651c12f9514da144dbc805145cc212abf3f5a15
Guilherme Martins <guimartins_9@hotmail.com>
2022-08-11 21:08:02 -0300

    Exercicio 6 - Crie um script para calcular a velocidade média de um carro numa corrida

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imcCalculator.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imcCalculator.js
@@ -1,8 +1,52 @@
+const readline = require('readline-sync');
+
 const imcCalculator = (weight, height) => {
   const heightInMeters = height / 100;
   const heightSquare = heightInMeters * heightInMeters;
   const imc = weight / heightSquare;
-  return imc.toFixed(2); 
+  return imc.toFixed(1); 
 };
 
-module.exports = imcCalculator;
+const IMC_MAXIMO_E_MINIMO_POR_SITUACAO = {
+  'Abaixo do peso (magreza)': {
+    imcMinimo: 0, // Um valor default mínimo qualquer, impossível de alcançar no imc.
+    imcMaximo: 18.4,
+  },
+  'Peso normal': {
+    imcMinimo: 18.5,
+    imcMaximo: 24.9,
+  },
+  'Acima do peso (sobrepeso)': {
+    imcMinimo: 25,
+    imcMaximo: 29.9,
+  },
+  'com Obesidade grau I': {
+    imcMinimo: 30.0,
+    imcMaximo: 34.9,
+  },
+  'com Obesidade grau II': {
+    imcMinimo: 35,
+    imcMaximo: 39.9,
+  },
+  'com Obesidade graus III e IV': {
+    imcMinimo: 40,
+    imcMaximo: 100 // Um valor default máximo qualquer, impossível de alcançar no imc.
+  },
+};
+
+const calculaSituacao = (imc) => {
+  const situacoes = Object.keys(IMC_MAXIMO_E_MINIMO_POR_SITUACAO);
+  const situacaoEncontrada = situacoes.find((situacao) => {
+    const { imcMinimo, imcMaximo } = IMC_MAXIMO_E_MINIMO_POR_SITUACAO[situacao];
+    return imc >= imcMinimo && imc <= imcMaximo;
+  })
+  return situacaoEncontrada;
+};
+
+const weight = readline.questionFloat('Qual o seu peso em KG? ');
+const height = readline.questionFloat('Qual a sua altura em CM? ');
+
+const imc = imcCalculator(weight, height);
+console.log(`Seu IMC é ${imc}`);
+const situacao = calculaSituacao(imc);
+console.log(`Você está ${situacao}`);
--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
+++
@@ -1,7 +0,0 @@
-const imcCalculator = require('./imcCalculator');
-const readline = require('readline-sync');
-
-const weight = readline.questionFloat('Qual o seu peso em KG? ');
-const height = readline.questionFloat('Qual a sua altura em CM? ');
-
-console.log(`Seu IMC é ${imcCalculator(weight, height)}`);
