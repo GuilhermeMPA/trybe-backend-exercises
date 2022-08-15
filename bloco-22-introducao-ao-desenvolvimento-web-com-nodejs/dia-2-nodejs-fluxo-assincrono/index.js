ce4519148e3c3ff107f2f616bd9732ab8999dc6f
Guilherme Martins <guimartins_9@hotmail.com>
2022-08-15 19:25:26 -0300

    1 - Crie uma função que receba três parâmetros e retorna uma Promise

---
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/index.js
@@ -0,0 +1,26 @@
+// 1 - Crie uma função que receba três parâmetros e retorna uma Promise.
+//       - Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números".
+//       - Caso todos os parâmetros sejam numéricos, some os dois primeiros e multiplique o resultado pelo terceiro ((a + b) * c).
+//       - Caso o resultado seja menor que 50, rejeite a Promise com o motivo "Valor muito baixo"
+//       - Caso o resultado seja maior que 50, resolva a Promise com o valor obtido.
+
+function mathExpression(a, b, c) {
+  return new Promise((resolve, reject) => {
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number') {
+      return reject(new Error('Informe apenas números'));
+    }
+
+    const result = (a + b) * c;
+    if (result < 50) {
+      return reject(new Error('Valor muito baixo'));
+    }
+
+    resolve(result);
+  });
+};
+
+
+
+mathExpression(1, 2, 3)
+  .then(resolve => console.log(resolve))
+  .catch(error => console.log(error.message));
