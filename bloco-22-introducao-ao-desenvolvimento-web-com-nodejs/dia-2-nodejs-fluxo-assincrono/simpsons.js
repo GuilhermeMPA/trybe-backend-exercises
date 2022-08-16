2143b0c2c8fe665eb11e79e24f5eeb6837809af7
Guilherme Martins <guimartins_9@hotmail.com>
2022-08-15 22:14:26 -0300

    Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -1,14 +1,14 @@
 const fs = require('fs').promises;
 
 async function readAllSimpsons() {
-  const simpsonsJson = await fs.readFile('./simpsons.json', 'utf8');
+  const simpsonsJson = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(simpsonsJson);
   const simpsonsByIdAndName = simpsons.map(({ id, name }) => `${id} - ${name}`);
   simpsonsByIdAndName.forEach((simpson) => console.log(simpson)); 
 }
 
 async function simpsonsById(id) {
-  const simpsonsJson = await fs.readFile('./simpsons.json', 'utf8');
+  const simpsonsJson = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(simpsonsJson);
   const choosenSimpson = simpsons.find((simpson) => Number(simpson.id) === id);
   if (!choosenSimpson) {
@@ -17,10 +17,19 @@ async function simpsonsById(id) {
   return choosenSimpson;
 };
 
+async function removeSimpson() {
+  const simpsonsJson = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(simpsonsJson);
+  const newSimpsons = simpsons.filter((simpson) => Number(simpson.id) !== 10 && Number(simpson.id) !== 6);
+  console.log(newSimpsons);
+  await fs.writeFile('./simpsons.json', JSON.stringify(newSimpsons));
+};
+
 async function main() {
   readAllSimpsons();
   const simpson = await simpsonsById(9);
   console.log(simpson);
+  await removeSimpson();
 };
 
 main();
