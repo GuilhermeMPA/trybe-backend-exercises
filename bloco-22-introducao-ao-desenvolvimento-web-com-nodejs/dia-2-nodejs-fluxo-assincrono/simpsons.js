04c9a29d656779d0b0d023fc4fcc1f7c4bc4d5bc
Guilherme Martins <guimartins_9@hotmail.com>
2022-08-15 22:20:42 -0300

    Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json, contendo as personagens com id de 1 a 5.

--- bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
+++ bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/simpsons.js
@@ -25,11 +25,19 @@ async function removeSimpson() {
   await fs.writeFile('./simpsons.json', JSON.stringify(newSimpsons));
 };
 
+async function simpsonsFamily() {
+  const simpsonsJson = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(simpsonsJson);
+  const simpsonsFamily = simpsons.filter((simpson) => simpson.id >= 1 && simpson.id <= 5);
+  await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily), { flag: 'wx' });
+};
+
 async function main() {
   readAllSimpsons();
   const simpson = await simpsonsById(9);
   console.log(simpson);
   await removeSimpson();
+  await simpsonsFamily();
 };
 
 main();
